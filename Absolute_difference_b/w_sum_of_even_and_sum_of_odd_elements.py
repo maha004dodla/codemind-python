f9fd85d585eae3ff1c2a1e025ea376b532e8d9a1
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-06 23:05:55 +0530

    n=int(input()) arr=list(map(int,input().split())) es=0 os=0 for i in range(n):     if(arr[i]%2==0):         es=es+arr[i]     if(arr[i]%2!=0):         os=os+arr[i] a=os-es import math print(abs(a))

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=list(map(int,input().split()))
+es=0
+os=0
+for i in range(n):
+    if(arr[i]%2==0):
+        es=es+arr[i]
+    if(arr[i]%2!=0):
+        os=os+arr[i]
+a=os-es
+import math
+print(abs(a))
