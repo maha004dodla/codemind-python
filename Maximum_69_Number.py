1af6270de3043ff14495646027c841bca7fcfa40
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-20 19:40:24 +0530

    a=int(input()) r,rev=0,0 d=0 while(a!=0):     b=a%10     a=a//10     r=r*10+b while(r!=0):     c=r%10     r=r//10     if(d<1):         if(c==6):             c=9             d=1     rev=rev*10+c print(rev)

---
+++ Maximum_69_Number.py
@@ -0,0 +1,16 @@
+a=int(input())
+r,rev=0,0
+d=0
+while(a!=0):
+    b=a%10
+    a=a//10
+    r=r*10+b
+while(r!=0):
+    c=r%10
+    r=r//10
+    if(d<1):
+        if(c==6):
+            c=9
+            d=1
+    rev=rev*10+c
+print(rev)
