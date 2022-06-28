eda251d9473099fb9f71941059f228313ad77dae
maha <97425755+maha004dodla@users.noreply.github.com>
2022-06-28 19:18:08 +0530

    n=int(input()) arr=list(map(int,input().split())) flag=0 for i in range(n):     if(arr[i]%2!=0 and i%2==0):         flag=1 if(flag==1):     print("False") else:     print("True")

---
+++ Strictly_ODD.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+flag=0
+for i in range(n):
+    if(arr[i]%2!=0 and i%2==0):
+        flag=1
+if(flag==1):
+    print("False")
+else:
+    print("True")
