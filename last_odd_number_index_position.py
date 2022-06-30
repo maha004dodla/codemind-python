a8b894ff66789980fe1659011b50500d6384add0
maha <97425755+maha004dodla@users.noreply.github.com>
2022-06-30 18:23:55 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 for i in range(n-1,1,-1):     if(arr[i]%2!=0):         print(i)         break

--- last_odd_number_index_position.py
+++ last_odd_number_index_position.py
@@ -1,7 +1,7 @@
 n=int(input())
 arr=list(map(int,input().split()))
-for i in range(n):
-    arr[i]=arr[n-1]
+c=0
+for i in range(n-1,1,-1):
     if(arr[i]%2!=0):
-        b=i
-print(b)
+        print(i)
+        break
