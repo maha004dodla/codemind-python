084907457ed68ea1bbb1e767333ed7507a6e40ed
maha <97425755+maha004dodla@users.noreply.github.com>
2022-08-23 11:24:48 +0530

    s=input() m=s.split(" ") for i in range(0,len(m)):     mini=ord(min(m[i]))     maxi=ord(max(m[i]))     print(abs(mini-maxi),end=" ")

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,6 +1,6 @@
 s=input()
-l=s.split()
-for i in l:
-    a=ord(min(i))
-    b=ord(max(i))
-    print(abs(a-b),end=" ")
+m=s.split(" ")
+for i in range(0,len(m)):
+    mini=ord(min(m[i]))
+    maxi=ord(max(m[i]))
+    print(abs(mini-maxi),end=" ")
