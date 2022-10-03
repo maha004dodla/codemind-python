6925fd48447abf1550b0c9ed0eae416f3cf42e40
maha <97425755+maha004dodla@users.noreply.github.com>
2022-10-03 09:37:35 +0530

    s=input() s1=input() s=list(s.lower()) s1=list(s1.lower()) a=[] b=[] flag=0 for i in s:     if i in s1 and (i!=' '):             a.append(i) for i in a:     if i not in b:         b.append(i)         flag=1 c=sorted(b) d="".join(c) if(flag==1):     print(d) else:     print('-1')

---
+++ common_characters.py
@@ -0,0 +1,20 @@
+s=input()
+s1=input()
+s=list(s.lower())
+s1=list(s1.lower())
+a=[]
+b=[]
+flag=0
+for i in s:
+    if i in s1 and (i!=' '):
+            a.append(i)
+for i in a:
+    if i not in b:
+        b.append(i)
+        flag=1
+c=sorted(b)
+d="".join(c)
+if(flag==1):
+    print(d)
+else:
+    print('-1')
