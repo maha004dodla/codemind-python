24d74f2570f4d0610eedf51213a90de205b1d374
maha <97425755+maha004dodla@users.noreply.github.com>
2022-10-11 18:38:28 +0530

    s=input() s=s.replace(" ","") s=s.lower() s1="" for i in s:     a=s.count(i)     if(a==1):         s1+=i b=sorted(s1) c="".join(b) print(c)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,7 +1,11 @@
 s=input()
-str=s.lower()
-str=str.replace(" ","")
-a=set(str)
-b=list(a)
-c=sorted(b)
-print("".join(c))
+s=s.replace(" ","")
+s=s.lower()
+s1=""
+for i in s:
+    a=s.count(i)
+    if(a==1):
+        s1+=i
+b=sorted(s1)
+c="".join(b)
+print(c)
