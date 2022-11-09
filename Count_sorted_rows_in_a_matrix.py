047b1a3c71460aee1c300156ae020fa4327d9971
maha <97425755+maha004dodla@users.noreply.github.com>
2022-11-09 15:55:24 +0530

    m,n=map(int,input().split()) l=[] for i in range(0,m):     d=0     arr=list(map(int,input().split()))     b=sorted(arr)     c=b[::-1]     if(arr==b or arr==c):         d+=1     l.append(d) print(sum(l))

--- Count_sorted_rows_in_a_matrix.py
+++ Count_sorted_rows_in_a_matrix.py
@@ -1,10 +1,10 @@
 m,n=map(int,input().split())
 l=[]
 for i in range(0,m):
+    d=0
     arr=list(map(int,input().split()))
     b=sorted(arr)
     c=b[::-1]
-    d=0
     if(arr==b or arr==c):
         d+=1
     l.append(d)
