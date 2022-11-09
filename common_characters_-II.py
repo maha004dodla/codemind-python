4b12a24276073b841be1145af2184aa9daec7eea
maha <97425755+maha004dodla@users.noreply.github.com>
2022-11-09 15:24:06 +0530

    a=input() b=input() a=a.lower() b=b.lower() a=list(a) b=list(b) l=[] k=[] for i in a:     if i in b and i!=" ":         l.append(i) for i in b:     if i in a and i!=" ":         l.append(i) for i in l:     if i not in k:         k.append(i) m="".join(k) a=set(m) print(len(a))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,13 +1,20 @@
-s=input()
-s1=input()
-s=list(s.lower())
-s1=list(s1.lower())
-a=[]
-b=[]
-for i in s:
-    if i in s1 and i!=' ':
-        a.append(i)
+a=input()
+b=input()
+a=a.lower()
+b=b.lower()
+a=list(a)
+b=list(b)
+l=[]
+k=[]
 for i in a:
-    if i not in b:
-        b.append(i)
-print(len(b))
+    if i in b and i!=" ":
+        l.append(i)
+for i in b:
+    if i in a and i!=" ":
+        l.append(i)
+for i in l:
+    if i not in k:
+        k.append(i)
+m="".join(k)
+a=set(m)
+print(len(a))
