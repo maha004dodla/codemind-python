b2f1d3a3b99277be55acb3e8321d8ff750b9aa68
maha <97425755+maha004dodla@users.noreply.github.com>
2022-11-28 15:25:18 +0530

    n=int(input()) arr=list(map(int,input().split())) e=[] o=[] for i in range(0,n):     if(arr[i]%2==0):         e.append(arr[i])     if(arr[i]%2!=0):         o.append(arr[i]) i,j=0,0 l=[] while(i<len(o) or j<len(e)):     if(i<len(o)):         l.append(o[i])     i+=1     if(j<len(e)):         l.append(e[j])     j+=1 if(len(l)%2!=0):     l.append(0) print(*l)

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -2,18 +2,20 @@ n=int(input())
 arr=list(map(int,input().split()))
 e=[]
 o=[]
-for i in range(n):
+for i in range(0,n):
     if(arr[i]%2==0):
         e.append(arr[i])
     if(arr[i]%2!=0):
         o.append(arr[i])
 i,j=0,0
-while(i<len(e) or j<len(o)):
-    if(j<len(o)):
-        print(o[j],end=" ")
-        j=j+1
-    if(i<len(e)):
-        print(e[i],end=" ")
-        i=i+1
-if(n%2!=0):
-    print("0")
+l=[]
+while(i<len(o) or j<len(e)):
+    if(i<len(o)):
+        l.append(o[i])
+    i+=1
+    if(j<len(e)):
+        l.append(e[j])
+    j+=1
+if(len(l)%2!=0):
+    l.append(0)
+print(*l)
