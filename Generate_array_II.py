ee515a474ba31de31cf193b9d01d851e27c78f8f
maha <97425755+maha004dodla@users.noreply.github.com>
2022-12-07 13:49:04 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] m=[] for i in range(len(arr)):     if(i%2!=0):         l.append(arr[i]) for i in range(len(arr)):     if(i%2==0):         m.append(arr[i]) n=[] for i in range(0,len(m)):     for j in range(0,l[i]):         n.append(m[i]) print(*n)

---
+++ Generate_array_II.py
@@ -0,0 +1,15 @@
+n=int(input())
+arr=list(map(int,input().split()))
+l=[]
+m=[]
+for i in range(len(arr)):
+    if(i%2!=0):
+        l.append(arr[i])
+for i in range(len(arr)):
+    if(i%2==0):
+        m.append(arr[i])
+n=[]
+for i in range(0,len(m)):
+    for j in range(0,l[i]):
+        n.append(m[i])
+print(*n)
